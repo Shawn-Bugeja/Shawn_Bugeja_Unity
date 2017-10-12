5d498744c7c5653ac7445d39caed3c31cc0da24f
Shawn Bugeja <b42227@mcast.org.mt>
2017-10-12 09:51:02 +0200

    starting prison break game

---
+++ prison break game/game/Assets/scripts/PrisonBreakTextGame.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PrisonBreakTextGame : MonoBehaviour {
+    public Text myText;
+
+    private enum States
+    {
+        cell, sheets_0, mirror, lock_0, cell_mirror, sheets_1, lock_1, freedom 
+    };
+
+    private States myState;
+
+	void Start () {
+        myState = States.cell;
+		
+	}
+
+    void Cell()
+    {
+        print("You are in a prison cell, and you want to escape. " +
+                "There are " + "some dirty sheets on the bed, a mirror on the wall, " +
+                "and the door " + "is locked from the outside.\n\n" + "Press S to view Sheets, " +
+                "M to view Mirror and L to view Lock");
+    }
+
+    void Update() {
+        print(myState);
+
+        if (myState == States.cell)
+        {
+            Cell();
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                myState = States.sheets_0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                myState = States.mirror;
+            }
+        }
+    }
+        
+}
